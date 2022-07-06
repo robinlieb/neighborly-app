909a868269c0fe7542ec836b00021bb4785fdd66
Robin Lieb <robin.j.lieb@gmail.com>
2022-07-06 18:28:04 +0200

    feat: added mongodb setup

--- NeighborlyAPI/getAdvertisements/__init__.py
+++ NeighborlyAPI/getAdvertisements/__init__.py
@@ -2,13 +2,14 @@ import azure.functions as func
 import pymongo
 import json
 from bson.json_util import dumps
+import os
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
 
     try:
-        url = "localhost"  # TODO: Update with appropriate MongoDB connection information
+        url = os.environ.get('CONNECTION_STRING')
         client = pymongo.MongoClient(url)
-        database = client['azure']
+        database = client['cosmos-neighborly']
         collection = database['advertisements']
 
 
--- NeighborlyAPI/getPosts/__init__.py
+++ NeighborlyAPI/getPosts/__init__.py
@@ -3,6 +3,7 @@ import azure.functions as func
 import pymongo
 import json
 from bson.json_util import dumps
+import os
 
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
@@ -10,9 +11,9 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python getPosts trigger function processed a request.')
 
     try:
-        url = "localhost"  # TODO: Update with appropriate MongoDB connection information
+        url = os.environ.get('CONNECTION_STRING')
         client = pymongo.MongoClient(url)
-        database = client['azure']
+        database = client['cosmos-neighborly']
         collection = database['posts']
 
         result = collection.find({})
